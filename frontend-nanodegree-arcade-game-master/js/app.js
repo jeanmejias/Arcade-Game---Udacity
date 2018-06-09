acafe6441f2fb0973ec4eeb753ef1fdfa35e32dc
jeanmejias <jean.eg.mejias@hotmail.com>
2018-06-09 15:52:49 +0100

    removed few functionalities and fix syntax

--- frontend-nanodegree-arcade-game-master/js/app.js
+++ frontend-nanodegree-arcade-game-master/js/app.js
@@ -1,3 +1,4 @@
+
 // Enemies our player must avoid
 class Enemy {
         constructor(sprite, x, y, speed) {
@@ -6,7 +7,7 @@ class Enemy {
             // The image/sprite for our enemies, this uses
             // a helper we've provided to easily load images
            
-        this.sprite = sprite; //enemy image
+        this.sprite = 'images/enemy-bug.png'; //enemy imagem
         this.x =x; // coordinate
         this.y =y; // coordinate
         this.speed = speed; //moving speed
@@ -33,12 +34,7 @@ checkCollision() {
         // moves the player back to it's start position
         player.x = 250;
         player.y = 498;
-        // changes the player image
-        player.sprite = 'images/char-boy.png';
-        // deletes one life
-        life --;
-        livesCount();
-    };
+         };
 }
 
 // Draw the enemy on the screen, required method for game
@@ -58,24 +54,11 @@ class Player {
 // This class requires an update(), render() and
 // a handleInput() method.
 update(dt) {
-    // if the player enters in collision with the colectable item
-    if (this.x === colectable.x & this.y === colectable.y) {
-        this.sprite = 'Gem Blue.png'; 
-        //moves the colectable item outside of the canvas
-        colectable.x = -101;
-        colectable.y = -83;
-    };
-    // checks if the player reaches the mountain
+        // checks if the player reaches the mountain
     if (this.y <= 83) {
-        //checks if the player has the colectable item
-        if (this.sprite ==='Gem Blue.png') {
-            achornCount(); // calls the colectable counter
-            this.sprite = 'images/enemy-bug.png';
-        };
-        this.y = 498; 
-        colectable.randomPosition();
-    };
+    }
 }
+
  //draws the player on the screen
 render() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
@@ -86,63 +69,35 @@ handleInput() {
     switch ( allowedKeys [e.keyCode]) {
         case 37: if (this.x >=101){
                     this.x = this.x -101;
-                     }; // moves the player to the left if it doesn't cross the canvas margin 
-         break;   
+                     } // moves the player to the left if it doesn't cross the canvas margin 
+         break; 
+           
         case 38: if (this.x <= 83 {
                 this.x = this.y -83;
-                    }; // moves the player up if it doesn't cross the canvas margin
+                    } // moves the player up if it doesn't cross the canvas margin
         break;
         case 39: if (this.x <= 404 {
             this.x = this.x + 101;
-                }; // moves the player to the right if it doesn't cross the canvas margin
+                } // moves the player to the right if it doesn't cross the canvas margin
         break;
         case 40: if (this.x <= 415 {
             this.y = this.y -83;
-                }; // moves the player down if it doesn't cross the canvas margin     
-        };
+                } // moves the player down if it doesn't cross the canvas margin     
+        }
     }
 };
-//colectable item class
-class colectable {
-    constructor (x,y) {
-        this.sprite = 'images/Gem Green.png';
-        this.x = x;
-        this.y = y;
-    }
-
-    //sets a random position for the colectable 
-    randomPosition() {
-        // sets a random x position
-        const minX = 0;
-        const maxX = 404;
-        this.x = Math.floor(Math.random() * (maxX - minX + 10)) + minX;
-        //sets a random Y position
-        const minY = 83;
-        const maxY = 415;
-        this.y = Math.floor(Math.random() * (maxY - minY + 10)) + minY;
-    }
-
-    //Draws the colectable on the sxreen 
-    render() {
-        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    }
-};
-
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 
-let enemy1 = new Enemy('images/enemy-bug.png', 120, 84, 3);
-let enemy2 = new Enemy('images/enemy-bug.png', 280, 167, 2);
-let enemy3 = new Enemy('images/enemy-bug.png', 0, 250, 5);
-let allEnemies = ['enemy1', 'enemy2', 'enemy3'];
+let enemy1 = new Enemy(-90, 60);
+let enemy2 = new Enemy(-190, 140);
+let enemy3 = new Enemy(-290, 230);
+let allEnemies = [enemy1, enemy2, enemy3];
 
 
 // Place the player object in a variable called player
 
-let player = new Player('images/char-boy.png', 250, 498);
-
-// Place the colectable object in a variable called colectable
-let colectable = new Colectable();
+let player = new Player('images/char-boy.png', 200, 300);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
